22fc189608c20422fa5f4c1ab9d6095bebd154de
DC286 <castrodaniel295@gmail.com>
2017-10-19 13:14:06 -0500

    I modifed the Main.grovy

--- Main.groovy
+++ Main.groovy
@@ -1,60 +1,65 @@
-List Room = [Room1, Room2, Room3, Room4, Room5]
-Room room1 =  Room{
-    RoomID = [1]
-    RoonName = [EastTowerRoom]
-    RoomDescription = ('This is Room ET')
 
-}
-Room room2 =  Room{
-    RoomID = [2]
-    RoomName = [Barracks]
-    RoomDescription = ('This is Room B')
-}
-Room  room3 =  Room{
-    RoomID = [3]
-    RoomName = [CentralTowerRoom]
-    roomDescription = ('This is Room CT')
-}
-Room  room4 = Room{
-    RoomID = [4]
-    RoomName = [WestTowerRoom]
-    RoomDescription = ('This is Room WT')
-}
-Room  Room5= Room{
-    roomID = [5]
-    RoomName = [Cellar]
-    RoomDescription = ('This is the room below Room B')
-}
-Map<Integer,Room> rooms = [1:Room1, 2:Room2, 3:Room4, 4:Room4, 5:Room5]
+Room startingRoom =  new Room(
+    roomID: 1,
+    title: "EastTowerRoom",
+    description:'This is Room ET'
+)
+
+Room roomTwo =  new Room (
+    roomID: 2,
+    title: "Barracks",
+    description: 'This is Room B'
+    )
+
+Room   roomThree =  new Room (
+    roomID: 3,
+    title: "CentralTowerRoom",
+    description: 'This is Room CT'
+    )
+
+Room  roomFour = new Room (
+    roomID: 4,
+    title: "WestTowerRoom",
+    description: 'This is Room WT'
+    )
+
+Room  roomFive = new Room (
+    roomID: 5,
+    title: "Cellar",
+    description: 'This is the room below Room B'
+    )
+
+Map<Integer,Room> rooms = [1:startingRoom, 2:roomTwo, 3:roomThree, 4:roomFour, 5:roomFive]
 
 
 
 
 
 Boolean keepPlaying = true
-while (keepPlaying) {
+while (keepPlaying){
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
-    print "Enter room number: "
-    def userIntput = br.readLine()
+        }
 
-    if (userInput == 'q'){
-    } else if (userInput) {
-        println "User's choice can't be blank. Please try again"
-    } else {
-        if (userIntput.Integer()) {
-            println "Please enter Room Number"
-        } else {
-            Room room = room1.get(userInput as Integer)
+    println "Enter room number: "
+    def userInput = br.readLine()
+
+    if (userInput == 'q') {
+        println "Quitting"
+        keepPlaying = false
+    }else if (userInput) {
+        println "Choice cannot be blank. Please try again"
+    }else if (userInput.isInput()) {
+        println "Enter room number"
+        else {
+            Room room = rooms.get(userInput as Integer)
 
             if (room) {
-                println "Welcome to " + room.ID
-
-                 {
-                 else  println ("Sorry, this room does not exist")
+                println "Welcome to " + room.title
+                else {
+                    println "Sorry, this room doesn't exist"
                 }
             }
-
-
         }
+
+
     }
-}
