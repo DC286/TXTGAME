5054c489de90611574f71614809e9189794a20d1
DC286 <castrodaniel295@gmail.com>
2017-11-02 16:51:49 -0500

    Initial Commit
    I changed the descriptions of the rooms and added map enforcement.

--- Main.groovy
+++ Main.groovy
@@ -1,71 +1,80 @@
 
 Room startingRoom =  new Room(
     roomID: 1,
-    title: "CastleEntrance",
-    description:'This is CE'
+    title: "The ruins of Hallsworth",
+
+    description: ''''You walk through the damaged gate. You look to your right and you see stairs lead up. You look to you left and you see a door.'''
 )
 
 Room roomTwo =  new Room (
     roomID: 2,
     title: "Barracks",
-    description: 'This is Room B'
+    description: ''' You've entered the barracks. There's 5 rows of beds. There's corpse's laying in some and in others are empty. In one bed lies a dented suit of armor.'''
+
     )
 
 Room   roomThree =  new Room (
     roomID: 3,
     title: "CentralTowerRoom",
-    description: 'This is Room CT'
+    description: '''You walk in and see what looks like the remains of a fight. There's a corpse with a missing head laying in the middle of the room. In the corpse's hand is a blood-stained sword.'''
     )
 
 Room  roomFour = new Room (
     roomID: 4,
     title: "WestTowerRoom",
-    description: 'This is Room WT'
+    description: '''You walk in and see a big hole in the wall. There's dried blood along the walls. In front of one of the arrow loops There is quiver with 5 arrows remaining and a bow.'''
     )
 
 Room  roomFive = new Room (
     roomID: 5,
-    title: "Cellar",
-    description: 'This is the room below Room B'
+    title: "Armory",
+    description: '''You enter the armory. You see empty racks that used to have weapons. On the wall in front of you there is a cracked shield'''
     )
 Room roomSix = new Room (
         roomID: 6,
         title: "SouthTowerRoom",
-        description: "Top floor of the East Tower"
+        description: '''You walk in see that there's barely any damage to the room and no gore or blood.'''
 )
-Map<Integer,Room> rooms = [1:startingRoom, 2:roomTwo, 3:roomThree, 4:roomFour, 5:roomFive, 6:roomSix]
 
 
-startingRoom.connections = ['e':CentralTowerRoom, 'w':WestTowerRoom]
-roomTwo.connections = ['s':EastTowerRoom, 'n':CentralTowerRoom]
-roomThree.connections = ['s':SouthTowerRoom, 'w':WestTowerRoom, 'se':Barracks]
-roomFour.connections = ['e':CentralTowerRoom, 's':CastleEntrance]
-roomFive.connections = ['s':WestTowerRoom]
-roomSix.connections = ['n':CenctralTowerRoom, 'ne':Barracks]
-
+startingRoom.connections = ['e':roomThree, 'w':roomFour]
+roomTwo.connections = ['s':roomSix, 'n':roomThree]
+roomThree.connections = ['s':roomSix, 'w':roomFour, 'se':roomTwo]
+roomFour.connections = ['e':roomThree]
+roomFive.connections = ['s':roomThree]
+roomSix.connections = ['n':roomThree, 'ne':roomTwo]
 
 Boolean keepPlaying = true
-while (keepPlaying){
+Room currentRoom = startingRoom
+
+while (keepPlaying) {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in))
+    println "This is the " + currentRoom.title
+    println currentRoom.description
 
-        }
 
-    println "Enter room number: "
     def userInput = br.readLine()
+      if (userInput == 'q') {
+          println "exiting game"
+          keepPlaying = false
 
-    if (userInput == 'q') {
-        println "Quitting"
-        keepPlaying = false
-    } else if (!userInput) {
-        println "Choice cannot be blank. Please try again"
-    } else if (userInput.isInput())
-        println "Enter room number"
- else {
-    Room room = room.get(UserInput as Integer)
 
-    if (room){
-        println "This Room doesn't exist"
-    }
+      } else {
+          Room room = currentRoom.connections.get(userInput)
+          if (room) {
+              currentRoom = room
+          }
+
+
+      }
+
+
+
+
+
+
 }
+
+
 
 
